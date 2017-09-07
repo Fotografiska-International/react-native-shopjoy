96db12d2344b70f2822495372331532923fb4884
Rameez Hussain <rameezh88@gmail.com>
2017-09-07 13:16:56 +0530

    Added remaining delegate methods.

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ type ShopJoyInitOptions = {
 type ShopJoyCallbacks = {
   shopJoyCampaignTrigged: Function,
   shopJoyReportsOutdatedCampaign: Function,
+  shopJoyQuestCompleted: ?Function,
+  shopJoyQuestPartlyCompleted: ?Function,
+  shopJoyReportsBluetoothState: ?Function,
+  shopJoyReportsBackgroundMode: ?Function,
 }
 
 const shopJoy = {
   initShopJoy: (options: ShopJoyInitOptions = {}) => {
     RNReactNativeShopjoy.initShopJoy(options.apiKey, options.userIdentifier);
   },
-  startListening: (callbacks: ShopJoyCallbacks = {}) => {
+  startListeningToCallbacks: (callbacks: ShopJoyCallbacks = {}) => {
     let eventEmitter = new NativeEventEmitter(RNReactNativeShopjoy);
     eventEmitter.addListener(RNReactNativeShopjoy.SHOP_JOY_CAMPAIGN_TRIGGERED, callbacks.shopJoyCampaignTrigged,
       reminder => console.log(reminder.name),
@@ -26,6 +30,28 @@ const shopJoy = {
     eventEmitter.addListener(RNReactNativeShopjoy.SHOP_JOY_REPORTS_OUTDATED_CAMPAIGN, callbacks.shopJoyReportsOutdatedCampaign,
       reminder => console.log(reminder.name),
     );
+
+    eventEmitter.addListener(RNReactNativeShopjoy.SHOP_JOY_QUEST_COMPLETED, callbacks.shopJoyQuestCompleted,
+      reminder => console.log(reminder.name),
+    );
+
+    eventEmitter.addListener(RNReactNativeShopjoy.SHOP_JOY_QUEST_PARTLY_COMPLETED, callbacks.shopJoyQuestPartlyCompleted,
+      reminder => console.log(reminder.name),
+    );
+
+    eventEmitter.addListener(RNReactNativeShopjoy.SHOP_JOY_REPORTS_BLUETOOTH_STATE, callbacks.shopJoyReportsBluetoothState,
+      reminder => console.log(reminder.name),
+    );
+
+    eventEmitter.addListener(RNReactNativeShopjoy.SHOP_JOY_REPORTS_BACKGROUND_MODE, callbacks.shopJoyReportsBackgroundMode,
+      reminder => console.log(reminder.name),
+    );
+  },
+  startMonitoring: () => {
+    RNReactNativeShopjoy.startMonitoring();
+  },
+  stopMonitoring: () => {
+    RNReactNativeShopjoy.stopMonitoring();
   }
 }
 
--- ios/RNReactNativeShopjoy.m
+++ ios/RNReactNativeShopjoy.m
@@ -4,6 +4,11 @@
 
 NSString *const kShopJoyCampaignTriggered = @"shopJoyCampaignTriggered";
 NSString *const kShopJoyReportsOutdatedCampaign = @"shopJoyReportsOutdatedCampaign";
+NSString *const kShopJoyQuestCompleted = @"shopJoyQuestCompleted";
+NSString *const kShopJoyQuestPartlyCompleted = @"shopJoyQuestPartlyCompleted";
+NSString *const kShopJoyReportsBluetoothState = @"shopJoyReportsBluetoothState";
+NSString *const kShopJoyReportsBackgroundMode = @"shopJoyReportsBackgroundMode";
+
 
 @interface RNReactNativeShopjoy()
 
@@ -23,11 +28,22 @@ RCT_EXPORT_MODULE()
 - (NSDictionary<NSString *, NSString *> *)constantsToExport {
     return @{ @"SHOP_JOY_CAMPAIGN_TRIGGERED": kShopJoyCampaignTriggered,
               @"SHOP_JOY_REPORTS_OUTDATED_CAMPAIGN": kShopJoyReportsOutdatedCampaign,
+              @"SHOP_JOY_QUEST_COMPLETED": kShopJoyQuestCompleted,
+              @"SHOP_JOY_QUEST_PARTLY_COMPLETED": kShopJoyQuestPartlyCompleted,
+              @"SHOP_JOY_REPORTS_BLUETOOTH_STATE": kShopJoyReportsBluetoothState,
+              @"SHOP_JOY_REPORTS_BACKGROUND_MODE": kShopJoyReportsBackgroundMode,
               };
 }
 
 - (NSArray<NSString *> *)supportedEvents {
-    return @[kShopJoyCampaignTriggered, kShopJoyReportsOutdatedCampaign];
+    return @[
+             kShopJoyCampaignTriggered,
+             kShopJoyReportsOutdatedCampaign,
+             kShopJoyQuestCompleted,
+             kShopJoyQuestPartlyCompleted,
+             kShopJoyReportsBluetoothState,
+             kShopJoyReportsBackgroundMode
+             ];
 }
 
 - (void)startObserving
@@ -52,22 +68,53 @@ RCT_EXPORT_METHOD(initShopJoy: (NSString *) apiKey userIdentifier: (NSString *) 
     self.shopJoyManager = [[ShopJoyManager alloc] initWithAPIKey:apiKey logLevel:ShopJoyLogLevelVerbose userIdentifier:userIdentifier idfa:nil delegate:self];
 }
 
+RCT_EXPORT_METHOD(startMonitoring)
+{
+    [self.shopJoyManager startMonitoring];
+}
+
+RCT_EXPORT_METHOD(stopMonitoring)
+{
+    [self.shopJoyManager stopMonitoring];
+}
+
 #pragma mark - Private methods.
 
 - (void)handleNotification:(NSNotification *)notification {
+    NSLog(@"%s %s", __PRETTY_FUNCTION__, __FUNCTION__);
     [self sendEventWithName:notification.name body:notification.userInfo];
 }
 
 #pragma mark - Delegate methods.
 
 - (void)shopJoyCampaignTriggered:(ShopJoyCampaign *)campaign {
-    NSLog(@"ShopJoy campaign triggered");
+    NSLog(@"%s %s", __PRETTY_FUNCTION__, __FUNCTION__);
     [self postNotificationName: kShopJoyCampaignTriggered withPayload: @{@"data" : @"test: campaign triggered"}];
 }
 
 - (void)shopJoyReportsOutdatedCampaign:(NSString *)campaignID {
-    NSLog(@"ShopJoy reports outdated campaign");
+    NSLog(@"%s %s", __PRETTY_FUNCTION__, __FUNCTION__);
     [self postNotificationName: kShopJoyReportsOutdatedCampaign withPayload: @{@"message" : @"Reporting outdated campaign", @"data": campaignID}];
+}
+
+- (void)shopJoyQuestCompleted:(ShopJoyQuest *)quest {
+    NSLog(@"%s %s", __PRETTY_FUNCTION__, __FUNCTION__);
+    [self postNotificationName: kShopJoyQuestCompleted withPayload: @{@"message" : @"Quest completed", @"data": quest}];
+}
+
+- (void)shopJoyQuestPartlyCompleted:(ShopJoyQuest *)quest {
+    NSLog(@"%s %s", __PRETTY_FUNCTION__, __FUNCTION__);
+    [self postNotificationName: kShopJoyQuestPartlyCompleted withPayload: @{@"message" : @"Quest partly completed", @"data": quest}];
+}
+
+- (void)shopJoyReportsBluetoothState:(Boolean)ready {
+    NSLog(@"%s %s", __PRETTY_FUNCTION__, __FUNCTION__);
+    [self postNotificationName: kShopJoyReportsBluetoothState withPayload: @{@"message" : @"Reports bluetooth state", @"data": @(ready)}];
+}
+
+- (void)shopJoyReportsBackgroundMode:(Boolean)enabled {
+    NSLog(@"%s %s", __PRETTY_FUNCTION__, __FUNCTION__);
+    [self postNotificationName: kShopJoyReportsBackgroundMode withPayload: @{@"message" : @"Reports background mode", @"data": @(enabled)}];
 }
 
 - (void)postNotificationName:(NSString *)name withPayload:(NSDictionary *)payload {
