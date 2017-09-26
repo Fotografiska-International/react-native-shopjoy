0f436a2a4623e0d96b364f6e4b0d534c1570aaa8
Rameez Hussain <rameezh88@gmail.com>
2017-09-26 12:57:01 +0530

    - Added constants. - Sending events to JS.

--- android/src/main/java/com/fotografiska/RNReactNativeShopjoyModule.java
+++ android/src/main/java/com/fotografiska/RNReactNativeShopjoyModule.java
@@ -2,11 +2,20 @@
 package com.fotografiska;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ObjectAlreadyConsumedException;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import se.injou.shopjoy.sdk.HistoryEntry;
 import se.injou.shopjoy.sdk.Quest;
@@ -16,8 +25,14 @@ import se.injou.shopjoy.sdk.ShopJoySDK;
 public class RNReactNativeShopjoyModule extends ReactContextBaseJavaModule {
 
     private static String TAG = "RNShopJoyModule";
-    private ReactApplicationContext reactContext;
+    private static ReactApplicationContext reactContext;
     private ShopJoySDK shopJoySDK;
+    private static String SHOP_JOY_CAMPAIGN_TRIGGERED = "SHOP_JOY_CAMPAIGN_TRIGGERED";
+    private static String SHOP_JOY_REPORTS_OUTDATED_CAMPAIGN = "SHOP_JOY_REPORTS_OUTDATED_CAMPAIGN";
+    private static String SHOP_JOY_QUEST_COMPLETED = "SHOP_JOY_QUEST_COMPLETED";
+    private static String SHOP_JOY_QUEST_PARTLY_COMPLETED = "SHOP_JOY_QUEST_PARTLY_COMPLETED";
+    private static String SHOP_JOY_REPORTS_BLUETOOTH_STATE = "SHOP_JOY_REPORTS_BLUETOOTH_STATE";
+    private static String SHOP_JOY_REPORTS_BACKGROUND_MODE = "SHOP_JOY_REPORTS_BACKGROUND_MODE";
 
     public RNReactNativeShopjoyModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -93,11 +108,28 @@ public class RNReactNativeShopjoyModule extends ReactContextBaseJavaModule {
         return "RNReactNativeShopjoy";
     }
 
+    @javax.annotation.Nullable
+    @Override
+    public Map<String, Object> getConstants() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(SHOP_JOY_CAMPAIGN_TRIGGERED, SHOP_JOY_CAMPAIGN_TRIGGERED);
+        return map;
+    }
+
+    private static void sendEvent(String eventName, @Nullable WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
     public static class RNReactNativeShopjoyCallbacks extends ShopJoyCallbacks {
         @Override
         public void onCampaignTriggered(Context context, HistoryEntry entry)
         {
             Log.d(TAG, "Campaign triggered");
+            WritableMap map = Arguments.createMap();
+
+            map.putString("message", "test: Campaign triggered");
+            sendEvent("SHOP_JOY_CAMPAIGN_TRIGGERED", map);
         }
         @Override
         public void onQuestTriggered(Context context, Quest quest) {
